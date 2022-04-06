78806b635f40433cf3d2f4ee3bacda89d181b20e
NhaT Tien <nguyenthanhhung@gmail.com>
2022-04-06 22:47:53 +0700

    Single Page Applications Part 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderMenuItem({ dish, onClick }) {
 	return (
 		<Card>
-			<CardImg width="100%" src={dish.image} alt={dish.name} />
-			<CardImgOverlay>
-				<CardTitle> {dish.name}</CardTitle>
-			</CardImgOverlay>
+				<CardImg width="100%" src={dish.image} alt={dish.name} />
+				<CardImgOverlay>
+					<CardTitle> {dish.name}</CardTitle>
+				</CardImgOverlay>
 		</Card>
 	);
 }
