7bf5b32568c909acbf90598667e6bf2d351845fc
biethereos <nguyenthanhhung@gmail.com>
2022-05-12 17:04:44 +0700

    update exercise2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderLeader() {
+function RenderLeader({leader}) {
     return(
-        <Media>
-            
-        </Media>
+        <div key={leader.id} className="col-12 mt-5">
+					<Media tag="li">
+						<Media left middle>
+							<Media object src={leader.image} alt={leader.name} />
+						</Media>
+						<Media body className="ml-5">
+							<Media heading> {leader.name}</Media>
+							<p>{leader.description}</p>
+						</Media>
+					</Media>
+				</div>
     );
 }
 
@@ -14,7 +22,7 @@ function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+            <RenderLeader leader={leader}/>
         );
     });
 
